68b01f2fe8188a715d2499f29ebc9e728668d256
mariamiah <mariah.kakande@gmail.com>
2020-11-29 16:12:47 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+import { listTodos } from './graphql/queries';
+import { updateTodo, createTodo } from './graphql/mutations';
+
+Amplify.configure(awsExports)
 
 function App() {
+
+  const [allTodos, setAllTodos ] = useState(null);
+  const [name, setTodoName] = useState("");
+
+  const changeTodoName = (e) => {
+    setTodoName(e.target.value)
+  }
+
+  const submitAddTodo = async(e) => {
+    e.preventDefault();
+    if(name === "") return alert("Input field empty");
+    const todo = { name, done: false};
+    await API.graphql(graphqlOperation(createTodo, {input:todo}));
+    allTodos === null ? setAllTodos([todo]) : setAllTodos([todo, ...allTodos]);
+  }
+  useEffect(() => {
+    (async() =>  {
+      const todos = await API.graphql(graphqlOperation(listTodos));
+      setAllTodos(todos.data.listTodos.items);
+    })()
+  }, [])
+  const toggleTodo = async(id) => {
+    const todo = allTodos.find(({id: _id}) => _id === id);
+    let newTodo = { id, name: todo.name};
+    newTodo.done = todo.done ? false : true;
+    await API.graphql(graphqlOperation(updateTodo, {input: newTodo}));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <h1>Amplify Todo</h1>
+        <div className="sign-out">
+          <AmplifySignOut />
+        </div>
+      </div>
+      <form className="add-todo-form" onSubmit={submitAddTodo}>
+        <input placeholder="Add ToDo"
+        onChange={changeTodoName}
+        />
+        <button type="submit">+</button>
+      </form>
+      {allTodos === null ? (
+        <p>Loading Todos...</p>
+      ): allTodos.length === 0 ? (
+        <p>No Todo Available</p>
+      ): (
+        <div className="todos">
+          {allTodos.reverse().map(({ id, name, done }, i) => (
+            <div className="todo-block" key={i}>
+              <input onClick={() => toggleTodo(id)} type="checkbox" id={id} value={id} key={i} defaultChecked={done} />
+              <label htmlFor={id}>{name}</label>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
